f8f7067b4c505f4910d86398d8b8397817963958
cazinge <eddie.azinge@gmail.com>
2019-05-15 16:01:54 -0700

    Creating project structure

--- mobile/App.js
+++ mobile/App.js
@@ -1,44 +1,43 @@
-import React from "react";
-import { Button, Image, FlatList, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { Button, Image, FlatList, StyleSheet, Text, View } from 'react-native';
 
-import { ApolloProvider, Query, Mutation } from "react-apollo";
+import { ApolloProvider, Query, Mutation } from 'react-apollo';
 
-import ApolloClient from "apollo-boost";
+import ApolloClient from 'apollo-boost';
 
-import { gql } from "apollo-boost";
-import { red } from "ansi-colors";
+import { gql } from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: 'http://localhost:4000/graphql',
 });
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 50
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 50,
   },
   row: {
     flex: 1,
-    alignSelf: "stretch",
-    flexDirection: "row",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 5
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 5,
   },
   list: {
-    alignSelf: "stretch",
+    alignSelf: 'stretch',
     marginLeft: 5,
-    marginRight: 5
+    marginRight: 5,
   },
   name: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginLeft: 5
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+  },
 });
 
 export default class App extends React.Component {
@@ -71,13 +70,13 @@ export default class App extends React.Component {
                 {data.characters.map(char => {
                   return (
                     <View
-                      style={{ flexDirection: "row", alignItems: "center" }}
+                      style={{ flexDirection: 'row', alignItems: 'center' }}
                     >
                       <Image
                         style={{ width: 50, height: 50 }}
                         source={{ uri: char.thumbnail }}
                       />
-                      <Text style={{ flex: 1, fontWeight: "bold" }}>
+                      <Text style={{ flex: 1, fontWeight: 'bold' }}>
                         {char.name}
                       </Text>
                     </View>
