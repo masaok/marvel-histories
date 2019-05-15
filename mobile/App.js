e7c27889a42c3e0be521a02fb76f4bdaff3c24e9
ZF <zach@keckmac4.local>
2019-05-14 17:30:50 -0700

    Added Thanos Image Placeholder

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app! TEST</Text>
+        <Image
+          style={{width: 620, height: 413}}
+          source={{uri: 'https://images.immediate.co.uk/volatile/sites/3/2018/03/DTT4430_v707.1023-61174bd.jpg?quality=90&resize=620,413'}}
+        />
       </View>
     );
   }
