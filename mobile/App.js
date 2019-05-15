97a77face884a7e275704f15d80a5af4bf956a23
Masao Kitamura <masaok@gmail.com>
2019-05-14 17:34:07 -0700

    basic graphql to get marvel characters

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { ApolloProvider, Query } from 'react-apollo';
+
+import ApolloClient from 'apollo-boost';
+
+import { gql } from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <Query
+          query={gql`
+            {
+              characters {
+                id
+                name
+                description
+                thumbnail
+              }
+            }
+          `}
+        >
+          {({ loading, data, error }) => {
+            console.log(loading, data && data.characters, error);
+            if (loading || error) {
+              return (
+                <View style={styles.container}>
+                  <Text>Loading...</Text>
+                </View>
+              );
+            }
+            return data.characters.map(character => (
+              <View style={styles.container}>
+                <Text>
+                  {character.name}, {character.thumbnail}
+                </Text>
+              </View>
+            ));
+          }}
+        </Query>
+      </ApolloProvider>
     );
   }
 }
@@ -16,6 +55,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+// https://www.apollographql.com/docs/react/essentials/get-started
