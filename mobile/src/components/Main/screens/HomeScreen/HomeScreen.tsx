e81c7fbb9bf2f14f56f09b2b9364b8aedcccdbbf
PokeADotProductions <zfitzpat@lion.lmu.edu>
2019-05-15 17:26:06 -0700

    Reorganizing files

--- mobile/src/components/Main/screens/HomeScreen/HomeScreen.tsx
+++ mobile/src/components/Main/screens/HomeScreen/HomeScreen.tsx
@@ -2,21 +2,59 @@
  * HomeScreen
  */
 
-import * as React from 'react';
-import { Text, View } from 'react-native';
-
-import styles from './HomeScreen.styles';
+import * as React from "react";
+import { Button, Image, FlatList, StyleSheet, Text, View } from "react-native";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import styles from "./HomeScreen.styles";
 
 export interface Props {}
 
 interface State {}
 
 export default class HomeScreen extends React.Component<Props, State> {
+  _keyExtractor = item => item.id;
   render() {
     return (
-      <View>
-        <Text>HomeScreen</Text>
-      </View>
+      <Query
+        query={gql`
+          {
+            characters(offset: 50) {
+              id
+              name
+              description
+              thumbnail
+            }
+          }
+        `}
+      >
+        {({ loading, data, error }) => {
+          if (loading || error) {
+            return (
+              <View style={styles.container}>
+                <Text>Loading...</Text>
+              </View>
+            );
+          }
+          return (
+            <FlatList
+              data={data.characters}
+              keyExtractor={this._keyExtractor}
+              renderItem={({ item }) => {
+                return (
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Image
+                      style={{ width: 50, height: 50 }}
+                      source={{ uri: item.thumbnail }}
+                    />
+                    <Text>{item.name}</Text>
+                  </View>
+                );
+              }}
+            />
+          );
+        }}
+      </Query>
     );
   }
 }
