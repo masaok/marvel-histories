7d6ff06112c4978cdbfcc273327c876fdb3bedd6
cazinge <eddie.azinge@gmail.com>
2019-05-15 19:03:42 -0700

    Fixing Characters endpoint

--- backend/MarvelQL/packages/graphql/index.ts
+++ backend/MarvelQL/packages/graphql/index.ts
@@ -4,15 +4,15 @@ import config from './config';
 import getContext from './utils/getContext';
 
 const server = new ApolloServer({
-	schema,
-	context: getContext,
-	engine: {
-		apiKey: config.engine.apiKey,
-		schemaTag: config.engine.schemaTag
-	},
-	playground: { version: '1.7.20' },
-	introspection: true
+  schema,
+  context: getContext,
+  engine: {
+    apiKey: config.engine.apiKey,
+    schemaTag: config.engine.schemaTag,
+  },
+  playground: { version: '1.7.20' },
+  introspection: true,
 });
 server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`);
 });
