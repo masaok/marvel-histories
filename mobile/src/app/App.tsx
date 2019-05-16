e7f527c8b2264949e411ebdd3f70dc9974ce215c
cazinge <eddie.azinge@gmail.com>
2019-05-15 18:16:58 -0700

    Fixing offset & limit

--- mobile/src/app/App.tsx
+++ mobile/src/app/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { ApolloProvider } from "react-apollo";
-import ApolloClient from "apollo-boost";
-import HomeScreen from "../components/Main/screens/HomeScreen";
-import SearchScreen from "../components/Main/screens/SearchScreen";
-import ViewCharacterScreen from "../components/Detail/screens/ViewCharacterScreen";
-import ViewComicScreen from "../components/Detail/screens/ViewComicScreen";
-import ViewSeriesScreen from "../components/Detail/screens/ViewSeriesScreen";
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import HomeScreen from '../components/Main/screens/HomeScreen';
+import SearchScreen from '../components/Main/screens/SearchScreen';
+import ViewCharacterScreen from '../components/Detail/screens/ViewCharacterScreen';
+import ViewComicScreen from '../components/Detail/screens/ViewComicScreen';
+import ViewSeriesScreen from '../components/Detail/screens/ViewSeriesScreen';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: 'http://localhost:4000/graphql',
 });
 
 export default class App extends React.Component {
