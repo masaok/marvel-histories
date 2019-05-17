f81892e8107f75cdfd59f38e34aade3b70b22722
cazinge <eddie.azinge@gmail.com>
2019-05-16 17:15:25 -0700

    Readding MainTabs

--- mobile/src/app/router.ts
+++ mobile/src/app/router.ts
@@ -1,22 +1,18 @@
-import { createAppContainer, createStackNavigator } from "react-navigation";
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
-import HomeScreen from "../components/Main/screens/HomeScreen";
-import SearchScreen from "../components/Main/screens/SearchScreen";
-import ViewCharacterScreen from "../components/Detail/screens/ViewCharacterScreen";
-import ViewComicScreen from "../components/Detail/screens/ViewComicScreen";
-import ViewSeriesScreen from "../components/Detail/screens/ViewSeriesScreen";
+import HomeScreen from '../components/Main/screens/HomeScreen';
+import SearchScreen from '../components/Main/screens/SearchScreen';
+import ViewCharacterScreen from '../components/Detail/screens/ViewCharacterScreen';
+import ViewComicScreen from '../components/Detail/screens/ViewComicScreen';
+import ViewSeriesScreen from '../components/Detail/screens/ViewSeriesScreen';
+import { MainTabsNavigator } from '../components/Main/router';
 
 const AppNavigator = createStackNavigator(
   {
-    Home: HomeScreen,
-    Search: SearchScreen,
-    ViewCharacter: ViewCharacterScreen,
-    ViewComic: ViewComicScreen,
-    ViewSeries: ViewSeriesScreen
+    Main: MainTabsNavigator,
   },
   {
-    initialRouteName: "ViewCharacter"
-    // headerMode: 'none',
+    initialRouteName: 'Main',
   }
 );
 
