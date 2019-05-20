90d95d3ca9ce10f0a2196253094bddaef3e63d17
Masao Kitamura <masaok@gmail.com>
2019-05-20 15:52:52 -0700

    Added Mutation to Character Timeline Screen to prepare to make save toggle button work

--- mobile/src/components/Timelines/screens/CharacterTimelineScreen/CharacterTimelineScreen.tsx
+++ mobile/src/components/Timelines/screens/CharacterTimelineScreen/CharacterTimelineScreen.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
 import { Text, View, FlatList, Image, Button } from 'react-native';
 
 import styles from './CharacterTimelineScreen.styles';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export interface Props { }
@@ -41,53 +41,64 @@ export default class CharacterTimelineScreen extends React.Component<
             );
           }
           return (
-            <View>
-              <Text>Add to Saved Timelines</Text>
-              <Button
-                onPress={() => {
-                  // TODO: this resolver function references a function in App.tsx, in the Apollo client
-                  // TODO: how do you import and call it here?
-                  toggleCharacterTimelineSave({
-                    variables: { character: item }
-                  })
-                }}
-                title="TEST"
-                // title={
-                //   data.likedCharacters
-                //     .map(char => char.id)
-                //     .indexOf(item.id) > -1
-                //     ? 'Unlike'
-                //     : 'Like'
-                // }
-                color='#841584'
-                accessibilityLabel='Learn more about this button'
-              />
-              <FlatList
-                data={data.comics}
-                keyExtractor={this._keyExtractor}
-                renderItem={({ item }) => {
+            <Mutation mutation={gql`
+                mutation TOGGLE_CHARACTER_TIMELINE_SAVE($character: Character!) {
+                  toggleCharacterTimelineSave(character: $character) @client
+                }
+              `}
+            >
+              {toggleCharacterTimelineSave => {
+                return (
+                  <View>
+                    <Text>Add to Saved Timelines</Text>
+                    <Button
+                      onPress={() => {
+                        // TODO: this resolver function references a function in App.tsx, in the Apollo client
+                        // TODO: how do you import and call it here?
+                        toggleCharacterTimelineSave({
+                          variables: { character: item }
+                        })
+                      }}
+                      title="TEST"
+                      // title={
+                      //   data.likedCharacters
+                      //     .map(char => char.id)
+                      //     .indexOf(item.id) > -1
+                      //     ? 'Unlike'
+                      //     : 'Like'
+                      // }
+                      color='#841584'
+                      accessibilityLabel='Learn more about this button'
+                    />
+                    <FlatList
+                      data={data.comics}
+                      keyExtractor={this._keyExtractor}
+                      renderItem={({ item }) => {
 
-                  // Parse and convert the date info for display
-                  const onsaleDate = item.dates[0].date
-                  const substring = onsaleDate.substring(0, 19)
-                  const obj = new Date(substring)
-                  const displayDay = obj.getDate() + 1
-                  const displayMonth = obj.getMonth() + 1
-                  const displayFullYear = obj.getFullYear() + 1
+                        // Parse and convert the date info for display
+                        const onsaleDate = item.dates[0].date
+                        const substring = onsaleDate.substring(0, 19)
+                        const obj = new Date(substring)
+                        const displayDay = obj.getDate() + 1
+                        const displayMonth = obj.getMonth() + 1
+                        const displayFullYear = obj.getFullYear() + 1
 
-                  return (
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                      <Text>{displayMonth}/{displayDay}/{displayFullYear}</Text>
-                      <Image
-                        style={{ width: 50, height: 50 }}
-                        source={{ uri: item.thumbnail }}
-                      />
-                      <Text>{item.title}</Text>
-                    </View>
-                  );
-                }}
-              />
-            </View>
+                        return (
+                          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>{displayMonth}/{displayDay}/{displayFullYear}</Text>
+                            <Image
+                              style={{ width: 50, height: 50 }}
+                              source={{ uri: item.thumbnail }}
+                            />
+                            <Text>{item.title}</Text>
+                          </View>
+                        );
+                      }}
+                    />
+                  </View>
+                )
+              }}
+            </Mutation>
           );
         }}
       </Query>
