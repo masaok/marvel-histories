b83c3695c03449ec3293ecc047fb6744b4845bee
Masao Kitamura <masaok@gmail.com>
2019-05-23 20:02:52 -0700

    Preparing My Timelines screen with mock data

--- mobile/src/components/Timelines/screens/MyTimelinesScreen/MyTimelinesScreen.tsx
+++ mobile/src/components/Timelines/screens/MyTimelinesScreen/MyTimelinesScreen.tsx
@@ -23,25 +23,38 @@ interface State {
     name: String,
     thumbnail: String,
   }
+  timelines:
+  {
+    id: Number,
+    name: String,
+    thumbnail: String,
+  }[][]
 }
 
 export default class MyTimelinesScreen extends React.Component<
   Props,
   State
   > {
+
   static navigationOptions = {
     header: props => {
       return (<SubScreenHeader title={"My Timelines"} {...props} />)
     }
   };
+
   _keyExtractor = item => item.id;
+
+  // _keyGenerator = item => {
+  //   id = id + 1
+  //   return id
+  // }
 
   constructor(props) {
     super(props);
 
     const characterParam = props.navigation.getParam("character");
 
-    // Mock
+    // Character Mock
     const characterMock = {
       id: 1009313,
       name: "Gambit",
@@ -51,15 +64,55 @@ export default class MyTimelinesScreen extends React.Component<
     // Default to incoming param, otherwise use the mock
     const character = characterParam ? characterParam : characterMock;
 
+    const timelinesMock = [
+      [
+        {
+          id: 1009313,
+          name: "Gambit",
+          thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/a/40/52696aa8aee99.jpg",
+        },
+        {
+          id: 1009159,
+          name: "Archangel",
+          thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/8/03/526165ed93180.jpg",
+        },
+        {
+          id: 1011012,
+          name: "Armadillo",
+          thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/2/40/4c0032754da02.jpg",
+        }
+      ],
+      [
+        {
+          id: 1009313,
+          name: "Gambit",
+          thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/a/40/52696aa8aee99.jpg",
+        },
+        {
+          id: 1011012,
+          name: "Armadillo",
+          thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/2/40/4c0032754da02.jpg",
+        }
+      ],
+      [
+        {
+          id: 1011012,
+          name: "Armadillo",
+          thumbnail: "http://i.annihil.us/u/prod/marvel/i/mg/2/40/4c0032754da02.jpg",
+        }
+      ]
+    ]
+
     this.state = {
       text: "Iron Man",
       name: null,
-      character
+      character,
+      timelines: timelinesMock
     };
   }
 
   render() {
-    const { character } = this.state
+    const { character, timelines } = this.state
 
     return (
       <Query
@@ -100,16 +153,41 @@ export default class MyTimelinesScreen extends React.Component<
                 console.log(data.savedCharacterTimelines)
                 return (
                   <View>
-                    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                    <View style={styles.listRow}>
+                      <Text style={styles.subtitle}>Character Timelines</Text>
+                    </View>
+                    <View style={styles.listRowOutline}>
                       <Image
                         style={styles.thumbnail}
                         source={{ uri: character.thumbnail }}
                       />
-                      <Text style={{
-                        marginLeft: 5,
-                        marginRight: "auto",
-                      }}>{character.name}</Text>
                     </View>
+                    <View style={styles.listRowOutline}>
+                      <Image
+                        style={styles.thumbnail}
+                        source={{ uri: character.thumbnail }}
+                      />
+                    </View>
+                    <View style={styles.listRowOutline}>
+                      <Image
+                        style={styles.thumbnail}
+                        source={{ uri: character.thumbnail }}
+                      />
+                    </View>
+                    <FlatList
+                      data={timelines}
+                      keyExtractor={this._keyExtractor}
+                      renderItem={({ item }) => {
+                        return (
+                          <View style={styles.listRowOutline}>
+                            <Image
+                              style={styles.thumbnail}
+                              source={{ uri: character.thumbnail }}
+                            />
+                          </View>
+                        );
+                      }}
+                    />
                   </View>
                 )
               }}
